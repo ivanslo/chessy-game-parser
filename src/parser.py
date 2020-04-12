f0b3ba637556e3ae0b6323c891ba44827960f404
Ivan Slobodiuk <ivanslo@yahoo.com.ar>
2020-04-12 21:57:37 +0100

    Add Basic Parsing

--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,10 @@
-from sly import Lexer
+from sly import Lexer, Parser
 import sys
 
+
+# -----------------------------------
+# Lexer
+# -----------------------------------
 class ChessLexer(Lexer):
 	tokens = { JUGADA_NRO, GAME_INFO, SPACE, PIECE, RANK, FILE, CASTLE1, CASTLE2, CHECK, TAKE, RESULT, PROMOTION }
 
@@ -24,10 +28,44 @@ class ChessLexer(Lexer):
 		print("Illegal character '%s'" % t.value[0])
 		self.index += 1
 
+# -----------------------------------
+# Parser
+# -----------------------------------
+class ChessParser(Parser):
+	tokens = ChessLexer.tokens
+	debugfile = 'parser.out'
+
+
+	@_('movements')
+	def game(self, p):
+		...
+	
+	@_('movements movement')
+	def movements(self, p):
+		print( p[1] )
+
+	@_('empty')
+	def movements(self, p):
+		...
+
+	@_('')
+	def empty(self,p):
+		pass
+
+	@_('SPACE', 'empty')
+	def whatever(self,p):
+		...
+
+	@_('JUGADA_NRO SPACE PIECE FILE RANK SPACE PIECE FILE RANK whatever')
+	def movement(self, p):
+		return ('movement', p.JUGADA_NRO, p.PIECE0, p.FILE0, p.RANK0, p.PIECE1, p.FILE1, p.RANK1)
+
+
+# -----------------------------------
+# Usage
+# -----------------------------------
+
 if __name__ == "__main__":
 	lexer = ChessLexer()
-	tokens = lexer.tokenize(sys.stdin.read())
-
-	for token in tokens:
-		print(token)
-
+	parser = ChessParser()
+	parser.parse(lexer.tokenize(sys.stdin.read()))
